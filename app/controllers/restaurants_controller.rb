98ea1602e162631bdbf6119822c5ecbb030f6322
olivier van den broek <olivier.vdbroek@gmail.com>
2018-11-21 12:35:00 +0100

    yelp 1

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,38 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :description, :stars)
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'japanese',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+  },
+  {
+    name:         'Le gorgeon',
+    address:      '23 av victor Hugo',
+    category:  'french',
+  },
+  {
+    name:         'Leon de Bruxelles',
+    address:      '12 rue du laos',
+    category:  'belgian',
+  },
+  {
+    name:         'Diepp',
+    address:      'avenue rodin',
+    category:  'chinese',
+  },
+  {
+    name:         'double dragon',
+    address:      'rue oberkampf',
+    category:  'chinese',
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
